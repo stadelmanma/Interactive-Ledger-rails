8d2d1efdfb4b4cfd6cb930739c38c0e5570c9465
Matthew Stadelman <stadelmanma@gmail.com>
2016-12-23 11:46:46 -0500

    Basic ledger pages are functional
    They will still need alot of polish

--- app/controllers/ledgers_controller.rb
+++ app/controllers/ledgers_controller.rb
@@ -4,4 +4,47 @@ class LedgersController < ApplicationController
     @ledgers = Ledger.all
   end
   
+  def show
+    @ledger = Ledger.find(params[:id])
+  end
+  
+  def new
+    @ledger = Ledger.new
+  end
+  
+  def edit
+    @ledger = Ledger.find(params[:id])
+  end
+  
+  def create
+    @ledger = Ledger.new(ledger_params)
+    
+    if @ledger.save
+      redirect_to @ledger
+    else
+      render 'new'
+    end
+  end
+  
+  def update
+    @ledger = Ledger.find(params[:id])
+    
+    if @ledger.update(ledger_params)
+      redirect_to @ledger
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @ledger = Ledger.find(params[:id])
+    @ledger.destroy
+    
+    redirect_to ledgers_path
+  end
+  
+  private
+  def ledger_params
+    params.require(:ledger).permit(:name, :data_source)
+  end
 end
--- app/models/ledger.rb
+++ app/models/ledger.rb
@@ -1,2 +1,4 @@
 class Ledger < ApplicationRecord
+  validates :name, presence: true
+  validates :data_source, presence: true
 end
