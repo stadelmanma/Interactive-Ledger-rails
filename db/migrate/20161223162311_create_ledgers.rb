5fca7282e729ac569c3d92bc5ac326a51abe5a40
Matthew Stadelman <stadelmanma@gmail.com>
2016-12-23 11:24:14 -0500

    Added ledger model which stores the Ledger name and data source

--- db/migrate/20161223162311_create_ledgers.rb
+++ db/migrate/20161223162311_create_ledgers.rb
@@ -1,6 +1,9 @@
 class CreateLedgers < ActiveRecord::Migration[5.0]
   def change
     create_table :ledgers do |t|
+      t.string :name
+      t.string :data_source
+      t.string :string
 
       t.timestamps
     end
