4c1ba7a58395e4b0a308f1bc0d4239a5cc34f577
Matthew Stadelman <stadelmanma@gmail.com>
2017-03-02 19:59:35 -0500

    Update model code to improve display
    Eventually I want to handle this in the view but for in the
    mean time formatting my numbers in the model will suffice.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,11 +1,18 @@
 class Transaction < ApplicationRecord
   belongs_to :ledger
-  
+
   # processes a tb delimited string
   def self.process_transaction_data(column_names, data)
     data = data.split(/\t/)
     data = Hash[column_names.zip data]
+    #
+    # process data values
     data['date'] = Date.strptime(data['date'].strip, '%m/%d/%y').to_s
+    data['amount'] = '%.2f' % Float(data['amount'].gsub(',', ''))
+    data['balance'].gsub!(/[[:space:]]/,'')
+    if !data['balance'].empty?
+        data['balance'] = '%.2f' % Float(data['balance'].gsub(',', ''))
+    end
     data['validated'] = data['validated'] == 'OK' ? 'YES' : 'NO'
     #
     return data
