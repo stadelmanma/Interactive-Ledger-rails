664e4e0c3603c87123ed9b1a8e3b0de15a9f707f
Matthew Stadelman <stadelmanma@gmail.com>
2017-03-16 16:09:43 -0400

    Move data upload logic from ledger into ledger_upload model

--- app/models/ledger.rb
+++ app/models/ledger.rb
@@ -1,31 +1,8 @@
 class Ledger < ApplicationRecord
   has_many :transactions, dependent: :destroy
+
+  has_many :ledger_uploads, dependent: :destroy
+  accepts_nested_attributes_for :ledger_uploads
+
   validates :name, presence: true
-  attr_accessor :data_source
-
-  # loading data from file specified by data_source attribute
-  def load_data
-    # loading initial data
-    path = Rails.root.to_s + '/' + data_source
-    data = File.read(path).split(/\n/)
-
-    # processing header row
-     column_names = Ledger.headers_to_column_names(data.shift)
-
-    # processing transaction data
-    data.map! do |row|
-      trans_data = Transaction.process_transaction_data(column_names, row)
-      trans_data[:ledger_id] = self.id
-      Transaction.new(trans_data)
-    end
-    #
-    return data
-  end
-
-  # processes header row to create valid attribute keys
-  def self.headers_to_column_names(header)
-    header = header.split(/\t/)
-    column_names = header.map {|name| name.downcase.strip}
-    column_names.map! {|name| name.sub /\s+/, '_'}
-  end
 end
--- app/models/ledger_upload.rb
+++ app/models/ledger_upload.rb
@@ -2,9 +2,43 @@ class LedgerUpload < ApplicationRecord
   belongs_to :ledger
   has_many :transactions, dependent: :destroy
 
+  validates :ledger, presence: true
   validates :data_source, presence: true
 
-  def self.create_from_ledger(ledger, params)
-    LedgerUpload.create!(params)
+  def upload_data
+    # process and upload new data
+    transactions = load_data
+    Transaction.import transactions
+    if ledger.persisted?
+      ledger.touch
+    end
+  end
+
+  private
+
+  def load_data
+    # loading raw data and splitting by new lines
+    path = Rails.root.to_s + '/' + data_source
+    data = File.read(path).split(/\n/)
+
+    # processing header row
+     column_names = Ledger.headers_to_column_names(data.shift)
+
+    # processing transaction data
+    data.map! do |row|
+      trans_data = Transaction.process_transaction_data(column_names, row)
+      trans_data[:ledger_id] = self.ledger.id
+      trans_data[:ledger_upload_id] = self.id
+      Transaction.new(trans_data)
+    end
+    #
+    return data
+  end
+
+  # processes header row to create valid attribute keys
+  def self.headers_to_column_names(header)
+    header = header.split(/\t/)
+    column_names = header.map {|name| name.downcase.strip}
+    column_names.map! {|name| name.sub /\s+/, '_'}
   end
 end
