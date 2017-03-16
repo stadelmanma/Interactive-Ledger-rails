573197020a3d0fc31848bbc25a6ce7d92d269d12
Matthew Stadelman <stadelmanma@gmail.com>
2017-03-15 20:13:29 -0400

    Refactor upload method in ledger model

--- app/controllers/ledgers_controller.rb
+++ app/controllers/ledgers_controller.rb
@@ -3,48 +3,64 @@ class LedgersController < ApplicationController
   def index
     @ledgers = Ledger.all
   end
-  
+
   def show
     @ledger = Ledger.find(params[:id])
-    @column_names, @data = @ledger.load_data
+    @column_names = Transaction.display_columns
   end
-  
+
   def new
-    @ledger = Ledger.new
+      @ledger = Ledger.new()
   end
-  
+
   def edit
     @ledger = Ledger.find(params[:id])
   end
-  
+
   def create
     @ledger = Ledger.new(ledger_params)
-    
+
     if @ledger.save
+      upload_data
       redirect_to @ledger
     else
       render 'new'
     end
   end
-  
+
   def update
     @ledger = Ledger.find(params[:id])
-    
+
     if @ledger.update(ledger_params)
+      upload_data
       redirect_to @ledger
     else
       render 'edit'
     end
   end
-  
+
   def destroy
     @ledger = Ledger.find(params[:id])
     @ledger.destroy
-    
+
     redirect_to ledgers_path
   end
-  
+
   private
+
+  def upload_data
+    # pull out data source param and return if it's not set
+    @ledger.data_source = params.require(:ledger)[:data_source]
+    return if @ledger.data_source.blank?
+
+    # process and upload new data
+    transactions = @ledger.load_data
+    Transaction.import transactions
+    if @ledger.persisted?
+      @ledger.touch
+    end
+  end
+
   def ledger_params
     params.require(:ledger).permit(:name, :data_source)
   end
--- app/models/ledger.rb
+++ app/models/ledger.rb
@@ -1,31 +1,31 @@
 class Ledger < ApplicationRecord
   has_many :transactions, dependent: :destroy
   validates :name, presence: true
-  validates :data_source, presence: true
-  
-  # processes header row to create valid attribute keys
-  def headers_to_column_names(header)
-    header = header.split(/\t/)
-    column_names = header.map {|name| name.downcase.strip}
-    column_names.map! {|name| name.sub /\s+/, '_'}
-  end
-  
+  attr_accessor :data_source
+
   # loading data from file specified by data_source attribute
   def load_data
     # loading initial data
     path = Rails.root.to_s + '/' + data_source
     data = File.read(path).split(/\n/)
-    
+
     # processing header row
-     column_names = headers_to_column_names(data.shift)
-    
+     column_names = Ledger.headers_to_column_names(data.shift)
+
     # processing transaction data
-    data.map! do |row| 
+    data.map! do |row|
       trans_data = Transaction.process_transaction_data(column_names, row)
-      #self.transactions.create(trans_data)
+      trans_data[:ledger_id] = self.id
+      Transaction.new(trans_data)
     end
     #
-    return column_names, data
+    return data
   end
-  
+
+  # processes header row to create valid attribute keys
+  def self.headers_to_column_names(header)
+    header = header.split(/\t/)
+    column_names = header.map {|name| name.downcase.strip}
+    column_names.map! {|name| name.sub /\s+/, '_'}
+  end
 end
