708fb1a772a16d295396e2430ce7223acd43b6da
Matthew Stadelman <stadelmanma@gmail.com>
2017-03-26 11:18:09 -0400

    Added simple calculation of totals (still needs more logic)

--- app/controllers/concerns/transaction_totals.rb
+++ app/controllers/concerns/transaction_totals.rb
@@ -15,15 +15,49 @@ module TransactionTotals
   end
 
   def create_totals_hash(ledger)
-    transactions = ledger.transactions
-    {
-      0 => {
-        rowspan: transactions.length,
-        week_total: 1,
-        total_deficit: 2,
-        category_totals: {'Test' => 0}
-      }
-    }
+    totals = {0 => {total_deficit: 0}}
+    cur_total = 0
+    #
+    # loop over all transactions
+    ledger.transactions.each_with_index do |transaction, i|
+      #
+      # create new entry for each week
+      if new_week?(ledger.transactions, i)
+        totals[i] = {
+          rowspan: 0,
+          week_total: 0,
+          total_deficit: totals[cur_total][:total_deficit],
+          category_totals: {}
+        }
+        cur_total = i
+      end
+      totals[cur_total][:rowspan] += 1
+      #
+      # increment totals
+      update_week_total(transaction, totals[cur_total])
+    end
+    #
+    return totals
+  end
+
+  def update_week_total(transaction, totals)
+    totals[:week_total] += transaction.amount
+    totals[:total_deficit] += transaction.amount
+  end
+
+  def update_category_totals(transaction)
+  end
+
+  # test if a date pair crosses a week boundary
+  def new_week?(transactions, i)
+    # return early if at beginning or end of list
+    return true if i == 0
+    return false if i + 1 >= transactions.length
+    #
+    curr_wk = transactions[i][:date].strftime("%U").to_i
+    test_wk = transactions[i + 1][:date].strftime("%U").to_i
+    # return true if week numbers are different
+    return curr_wk != test_wk
   end
 
 end
