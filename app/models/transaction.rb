fcf70e324cba908dfdba4214dadd79ef2e2fd6da
Matthew Stadelman <stadelmanma@gmail.com>
2017-05-06 14:44:46 -0400

    Rubocop fixes in transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,4 @@
+# stores the information for a single monetary transaction
 class Transaction < ApplicationRecord
   include ActionView::Helpers::NumberHelper
 
@@ -13,43 +14,32 @@ class Transaction < ApplicationRecord
     #
     # process data values
     data['date'] = Date.strptime(data['date'].strip, '%m/%d/%y').to_s
-    data['amount'] = '%.2f' % Float(data['amount'].gsub(',', ''))
-    data['balance'].gsub!(/[[:space:]]/,'')
-    if !data['balance'].empty?
-        data['balance'] = '%.2f' % Float(data['balance'].gsub(',', ''))
+    data['amount'] = format('%.2f', Float(data['amount'].delete(',')))
+    data['balance'].gsub!(/[[:space:]]/, '')
+    unless data['balance'].empty?
+      data['balance'] = format('%.2f', Float(data['balance'].delete(',')))
     end
     data['validated'] = data['validated'] == 'OK' ? true : false
     #
-    return data
+    data
   end
 
   # returns an array of column names to display in order
   def self.display_columns
-    [
-      'date',
-      'description',
-      'amount',
-      'balance',
-      'account',
-      'validated',
-      'category',
-      'subcategory',
-      'comments'
-    ]
+    %w[date description amount balance account validated
+       category subcategory comments]
+  end
+
+  def self.display_number(number)
+    helper = Object.new.extend(ActionView::Helpers::NumberHelper)
+    helper.number_with_precision(number, delimiter: ',', precision: 2)
   end
 
   # returns a formatted hash for display
   def gen_display_hash
-    @display_data = attributes.to_h()
+    @display_data = attributes.to_h
     @display_data['amount'] = Transaction.display_number(amount)
     @display_data['balance'] = Transaction.display_number(balance)
     @display_data['validated'] = validated ? 'YES' : 'NO'
   end
-
-
-  def self.display_number(number)
-    helper = Object.new.extend(ActionView::Helpers::NumberHelper)
-    helper.number_with_precision(number, :delimiter => ',', :precision => 2)
-  end
-
 end
