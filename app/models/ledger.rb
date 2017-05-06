ae1b5319bcc53431e8cb04812075b47c3e75a419
Matthew Stadelman <stadelmanma@gmail.com>
2017-05-06 14:44:25 -0400

    Rubocop fixes in ledger model

--- app/models/ledger.rb
+++ app/models/ledger.rb
@@ -1,16 +1,18 @@
+# Stores several transactions to track expenses
 class Ledger < ApplicationRecord
   has_many :transactions, dependent: :destroy
 
   has_many :ledger_uploads, inverse_of: :ledger, dependent: :destroy
   accepts_nested_attributes_for :ledger_uploads,
-    :reject_if => proc { |attributes| attributes['data_source'].blank? }
+                                reject_if: proc { |attributes|
+                                  attributes['data_source'].blank?
+                                }
 
   validates_associated :ledger_uploads
   validates :name, presence: true
 
   def upload_data
-    if ledger_uploads.last then
-      ledger_uploads.last.upload_data
-    end
+    return unless ledger_uploads.last
+    ledger_uploads.last.upload_data
   end
 end
