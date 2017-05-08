bfb290dcc4e507425fdee1cff0269df3f979fac1
Matthew Stadelman <stadelmanma@gmail.com>
2017-05-07 21:35:41 -0400

    Delete unused files from app

--- app/helpers/interactive_financial_ledger_helper.rb
+++
@@ -1,2 +0,0 @@
-module InteractiveFinancialLedgerHelper
-end
--- test/controllers/interactive_financial_ledger_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class InteractiveFinancialLedgerControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get interactive_financial_ledger_index_url
-    assert_response :success
-  end
-
-end
