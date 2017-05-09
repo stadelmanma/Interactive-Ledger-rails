f1be07307d05238c6b46fa723f583f14f657141f
Matthew Stadelman <stadelmanma@gmail.com>
2017-05-09 15:49:28 -0400

    Refactor ledger_upload_helpder so different formats are their own class
    This allows for more complex logic need be although the current implementation
    could still operate 'functionally'.

--- app/helpers/ledger_upload_helper.rb
+++ app/helpers/ledger_upload_helper.rb
@@ -2,68 +2,108 @@ require 'csv'
 
 # Handles uploading data from different formats
 module LedgerUploadHelper
-
-  # uploads for each format, as well as defaults
+  # Uploads the data from a file using a block as the last arg to build
+  # each Transaction object.
   def upload_from_format(data_file, data_format)
-    case data_format.downcase
-    when 'discover'
-      delim, date_fmt, column_mapping = discover
-    else
-      delim = "\t"
-      column_mapping = {}
-      date_fmt = '%m/%d/%y'
-    end
-
-    # add converter for commma separated numbers
-    comma_numbers = ->(s) { s =~ /^\d+,/ ? s.delete(',').to_f : s }
-    date_conversion = lambda { |s|
-      begin
-        Date.strptime(s, date_fmt)
-      rescue ArgumentError
-        s
-      end
-    }
-    CSV::Converters[:comma_numbers] = comma_numbers
-    CSV::Converters[:date_conversion] = date_conversion
+    #
+    # determine data upload format
+    formatter = get_formatter(data_format)
 
     # processing the data file
     transactions = []
-    CSV.foreach(data_file,
-                quote_char: '"',
-                col_sep: delim,
-                converters: %i[all comma_numbers date_conversion],
-                headers: true,
-                header_converters: :symbol) do |row|
-                  next if row.empty?
-                  row = map_columns(row, column_mapping)
-                  transactions.push(yield(row))
-                end
+    CSV.foreach(data_file, formatter.csv_kwargs) do |row|
+      row = formatter.map_columns(row)
+      transactions.push(yield(row))
+    end
     transactions
   end
 
-  def map_columns(data, column_mapping)
-    mapped_data = {}
-    data.map do |key, value|
-      # remove nil keys from extra empty columns in file
-      next if key.nil?
-      # unmapped keys are passed through, if column_mapping[key] is nil
-      # then the value is not stored in the mapping
-      if column_mapping.key?(key)
-        mapped_data[column_mapping[key]] = value if column_mapping[key]
-      else
-        mapped_data[key] = value
-      end
+  def get_formatter(data_format)
+    case data_format.downcase
+    when 'discover'
+      DiscoverFormat.new
+    else
+      DefaultFormat.new
     end
-    mapped_data
   end
 
-  # Different column name mappings and delimiter for each upload_format
-  def discover #  this would be best stored in the database eventually
-    mapping = {
-      trans_date: :date,
-      post_date: nil
-    }
-    #
-    return ',', '%m/%d/%Y', mapping
+  # default class for handling file uploads
+  class DefaultFormat
+    attr_reader :csv_kwargs
+    DEFAULT_KWARGS = {
+      quote_char: '"',
+      col_sep: "\t",
+      skip_blanks: true,
+      converters: :all,
+      headers: true,
+      header_converters: :symbol
+    }.freeze
+
+    def initialize
+      @date_fmt = '%m/%d/%y'
+      @csv_kwargs = DEFAULT_KWARGS.merge(converters: add_converters)
+      @column_name_mapping = {}
+      @column_value_mapping = {}
+    end
+
+    def map_columns(data)
+      mapped_data = {}
+      data.map do |column, value|
+        # remove nil keys from extra empty columns in file
+        next if column.nil?
+
+        # unmapped keys are passed through, if column_mapping[key] is nil
+        # then the value is not stored in the mapping
+        key = column
+        if @column_name_mapping.key?(column)
+          next if @column_name_mapping[column].nil?
+          key = @column_name_mapping[column]
+        end
+
+        # processing values as needed
+        if @column_value_mapping.key?(column)
+          value = @column_value_mapping[column].call(value)
+        end
+
+        mapped_data[key] = value
+      end
+      mapped_data
+    end
+
+    private
+
+    # adds converts to the list and returns a list of symbols to use
+    def add_converters
+      # add converter for commma separated numbers
+      comma_numbers = ->(s) { s =~ /^\d+,/ ? s.delete(',').to_f : s }
+
+      # handle date parsing gracefully
+      date_conversion = lambda { |s|
+        begin
+          Date.strptime(s, @date_fmt)
+        rescue ArgumentError
+          s
+        end
+      }
+      #
+      CSV::Converters[:comma_numbers] = comma_numbers
+      CSV::Converters[:date_conversion] = date_conversion
+      %i[all comma_numbers date_conversion]
+    end
+  end
+
+  # Additional logic to properly map column names and values
+  class DiscoverFormat < DefaultFormat
+    def initialize
+      @date_fmt = '%m/%d/%Y'
+      @csv_kwargs = { col_sep: ',', converters: add_converters }
+      @csv_kwargs = DEFAULT_KWARGS.merge(@csv_kwargs)
+      @column_name_mapping = {
+        trans_date: :date,
+        post_date: nil,
+        category: :comments
+      }
+      @column_value_mapping = { amount: ->(v) { -v } }
+    end
   end
 end
