952911161e8c40b814819d87f7c9993f01a105c1
Matthew Stadelman <stadelmanma@gmail.com>
2017-05-13 09:24:15 -0400

    Add logic to parse Chessie formatted data
    Also refactored helper structure

--- app/helpers/ledger_upload_helper.rb
+++ app/helpers/ledger_upload_helper.rb
@@ -22,8 +22,12 @@ module LedgerUploadHelper
     case data_format.downcase
     when 'discover'
       DiscoverFormat.new
-    else
+    when 'chessie'
+      ChessieFormat.new
+    when 'default'
       DefaultFormat.new
+    else
+      throw ArgumentError("Unkown data format: #{data_format}")
     end
   end
 
@@ -40,7 +44,7 @@ module LedgerUploadHelper
     }.freeze
 
     def initialize
-      @date_fmt = '%m/%d/%y'
+      @date_fmt = '%m/%d/%Y'
       @csv_kwargs = DEFAULT_KWARGS.merge(converters: add_converters)
       @column_name_mapping = {}
       @column_value_mapping = {}
@@ -54,18 +58,11 @@ module LedgerUploadHelper
 
         # unmapped keys are passed through, if column_mapping[key] is nil
         # then the value is not stored in the mapping
-        key = column
-        if @column_name_mapping.key?(column)
-          next if @column_name_mapping[column].nil?
-          key = @column_name_mapping[column]
-        end
+        key = process_column_names(column)
+        next if key.nil?
 
         # processing values as needed
-        if @column_value_mapping.key?(column)
-          value = @column_value_mapping[column].call(value)
-        end
-
-        mapped_data[key] = value
+        mapped_data[key] = process_column_values(column, value)
       end
       mapped_data
     end
@@ -75,7 +72,14 @@ module LedgerUploadHelper
     # adds converts to the list and returns a list of symbols to use
     def add_converters
       # add converter for commma separated numbers
-      comma_numbers = ->(s) { s =~ /^\d+,/ ? s.delete(',').to_f : s }
+      account_numbers = lambda { |s|
+        begin
+          val = Float(s.gsub(/[$,()]/, ''))
+          s =~ /^\(.+\)$/ ? -val : val
+        rescue ArgumentError
+          s
+        end
+        }
 
       # handle date parsing gracefully
       date_conversion = lambda { |s|
@@ -86,16 +90,44 @@ module LedgerUploadHelper
         end
       }
       #
-      CSV::Converters[:comma_numbers] = comma_numbers
+      CSV::Converters[:account_numbers] = account_numbers
       CSV::Converters[:date_conversion] = date_conversion
-      %i[all comma_numbers date_conversion]
+      %i[all account_numbers date_conversion]
+    end
+
+    def process_column_names(column)
+      if @column_name_mapping.key?(column)
+        @column_name_mapping[column]
+      else
+        column
+      end
+    end
+
+    def process_column_values(column, value)
+      if @column_value_mapping.key?(column)
+        @column_value_mapping[column].call(value)
+      else
+        value
+      end
+    end
+  end
+
+  # Additional logic to properly map column names and values
+  class ChessieFormat < DefaultFormat
+    def initialize
+      super
+      @date_fmt = '%m/%d/%Y'
+      @csv_kwargs = { col_sep: ',', converters: add_converters }
+      @csv_kwargs = DEFAULT_KWARGS.merge(@csv_kwargs)
+      @column_name_mapping = { effective_date: :date }
+      @column_value_mapping = {}
     end
   end
 
   # Additional logic to properly map column names and values
   class DiscoverFormat < DefaultFormat
     def initialize
-      @date_fmt = '%m/%d/%Y'
+      super
       @csv_kwargs = { col_sep: ',', converters: add_converters }
       @csv_kwargs = DEFAULT_KWARGS.merge(@csv_kwargs)
       @column_name_mapping = {
