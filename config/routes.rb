7bc1205a68f26532e298884fd06a85488eb283be
Matthew Stadelman <stadelmanma@gmail.com>
2017-06-04 19:17:01 -0400

    Add route to download the ledger

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :ledger_uploads
   end
 
+  get 'ledgers/:id/:download', to: 'ledgers#download'
+
   root 'ledgers#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
