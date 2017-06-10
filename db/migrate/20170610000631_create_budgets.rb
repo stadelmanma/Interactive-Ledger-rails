7a4cbaa0f167aec81fe46016deb16767688addd9
Matthew Stadelman <stadelmanma@gmail.com>
2017-06-09 20:52:24 -0400

    Update database to include budgets table

--- db/migrate/20170610000631_create_budgets.rb
+++ db/migrate/20170610000631_create_budgets.rb
@@ -1,7 +1,10 @@
 class CreateBudgets < ActiveRecord::Migration[5.0]
   def change
     create_table :budgets do |t|
-
+      t.date :date
+      t.string :description
+      t.float :amount
+      t.text :comments
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170325152236) do
+ActiveRecord::Schema.define(version: 20170610000631) do
+
+  create_table "budgets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.date     "date"
+    t.string   "description"
+    t.float    "amount",      limit: 24
+    t.text     "comments",    limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "ledger_uploads", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "ledger_id"
