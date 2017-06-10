ec7eb6f9636196eca99cadc8ea8650193d711f92
Matthew Stadelman <stadelmanma@gmail.com>
2017-06-09 20:37:23 -0400

    Add budget model to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'interactive_financial_ledger/index'
 
+  resources :budgets
+
   resources :ledgers do
     resources :ledger_uploads do
       get 'download', on: :member
