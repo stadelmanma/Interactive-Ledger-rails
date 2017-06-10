2426f29dd8c0416094fd7d3df3377c1653d6894b
Matthew Stadelman <stadelmanma@gmail.com>
2017-06-09 20:24:53 -0400

    Ads basic CRUD methods to budget controller

--- app/controllers/budget_controller.rb
+++ app/controllers/budget_controller.rb
@@ -1,2 +1,51 @@
+# A budget model to compare expected expenses with actual transactions
 class BudgetController < ApplicationController
+  def index
+    @budgets = Budget.all
+  end
+
+  def show
+    @budget = Budget.find(params[:id])
+  end
+
+  def new
+    @budget = Budget.new
+  end
+
+  def edit
+    @budget = Budget.find(params[:id])
+  end
+
+  def create
+    @budget = Budget.new(budget_params)
+    #
+    if @budget.save
+      redirect_to @budget
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @budget = Budget.find(params[:id])
+    #
+    if @budget.update(budget_params)
+      redirect_to @budget
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @budget = Budget.find(params[:id])
+    @budget.destroy
+
+    redirect_to budgets_path
+  end
+
+  private
+
+  def budget_params
+    params.require(:budget)
+  end
 end
