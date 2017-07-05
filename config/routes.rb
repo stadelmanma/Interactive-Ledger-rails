5f3b3caec017cc7bacb9cb35d4211a99c03904a5
Matthew Stadelman <stadelmanma@gmail.com>
2017-07-04 21:36:31 -0400

    Add route to return nested partial html to ajax query

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'interactive_financial_ledger/index'
 
-  resources :budgets
+  resources :budgets do
+    get 'add-budget-ledger-select' => 'budgets#add_budget_ledger_select',
+        on: :collection
+  end
 
   resources :ledgers do
     resources :ledger_uploads do
