f1cec74546aac16e08337aaba5d2734313d84c6a
Matthew Stadelman <stadelmanma@gmail.com>
2017-07-04 21:48:50 -0400

    Update budget model to accept nested attributes and allow deletion

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,11 +1,14 @@
 # Combines planned expenses with actual transactions recorded
 class Budget < ApplicationRecord
   has_many :budget_expenses, inverse_of: :budget, dependent: :destroy
-  accepts_nested_attributes_for :budget_expenses
+  accepts_nested_attributes_for :budget_expenses, allow_destroy: true,
+                                                  reject_if: :all_blank
 
   has_many :budget_ledgers
   has_many :ledgers, through: :budget_ledgers
+  accepts_nested_attributes_for :budget_ledgers, allow_destroy: true,
+                                                 reject_if: :all_blank
 
-  validates_associated :budget_expenses
   validates :name, presence: true
+  validates_associated :budget_expenses
 end
