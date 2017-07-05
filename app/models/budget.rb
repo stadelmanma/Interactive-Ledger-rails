36cd66049f448d033b8fae8fe1528a00cba4f53c
Matthew Stadelman <stadelmanma@gmail.com>
2017-07-04 21:57:25 -0400

    Fix budget model so it can be saved at the same time as an associated ledger

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -4,11 +4,12 @@ class Budget < ApplicationRecord
   accepts_nested_attributes_for :budget_expenses, allow_destroy: true,
                                                   reject_if: :all_blank
 
-  has_many :budget_ledgers
+  has_many :budget_ledgers, inverse_of: :budget, dependent: :destroy
   has_many :ledgers, through: :budget_ledgers
   accepts_nested_attributes_for :budget_ledgers, allow_destroy: true,
                                                  reject_if: :all_blank
 
   validates :name, presence: true
+  validates_associated :budget_ledgers
   validates_associated :budget_expenses
 end
