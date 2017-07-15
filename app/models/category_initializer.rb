e2ab92d9c61b33a8ff42127ddeb61abbac9ddf86
Matthew Stadelman <stadelmanma@gmail.com>
2017-07-14 21:08:56 -0400

    Add basic validations to category initializers model

--- app/models/category_initializer.rb
+++ app/models/category_initializer.rb
@@ -1,2 +1,5 @@
+# Uses a pattern to test the description and set a default category
 class CategoryInitializer < ApplicationRecord
+  validates :pattern, presence: true
+  validates :category, presence: true
 end
