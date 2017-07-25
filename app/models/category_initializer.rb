1ddc71b9d9914bdaf658f513fae3517a6808ea1c
Matthew Stadelman <stadelmanma@gmail.com>
2017-07-24 20:38:28 -0400

    Add pattern validator and find initializer methods to Category Initalizer class

--- app/models/category_initializer.rb
+++ app/models/category_initializer.rb
@@ -1,7 +1,26 @@
 # Uses a pattern to test the description and set a default category
 class CategoryInitializer < ApplicationRecord
+  before_validation do
+    self.pattern = pattern.strip if attribute_present?('pattern')
+  end
+
   validates :pattern, presence: true
+  validate :check_pattern
   validates :category, presence: true
 
   attr_accessor :_destroy
+
+  def self.find_initializer(description)
+    CategoryInitializer.all.order(priority: :desc).detect do |ci|
+      description =~ Regexp.new(ci.pattern, true)
+    end
+  end
+
+  private
+
+  def check_pattern
+    Regexp.new(pattern)
+  rescue RegexpError => e
+    errors.add(:pattern, e.message)
+  end
 end
