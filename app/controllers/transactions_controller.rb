4c089a96ea0457a7600f66643016df9bc7c15b24
Matthew Stadelman <stadelmanma@gmail.com>
2017-08-07 20:21:34 -0400

    Create duplicates view

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,12 +5,22 @@ class TransactionsController < ApplicationController
   def show; end
 
   def duplicates
+    @page_links = [
+      { name: 'Ledger Upload',
+        url: [@transaction.ledger, @transaction.ledger_upload] }
+    ]
     @column_names = Transaction.display_columns
   end
 
   def destroy
+    url = if request.referer.include?("/transactions/#{@transaction.id}")
+            @transaction.ledger_upload
+          else
+            :back
+          end
+    #
     @transaction.destroy
-    redirect_to @transaction.ledger_upload
+    redirect_to url
   end
 
   private
