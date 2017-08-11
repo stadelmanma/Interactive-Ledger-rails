331c27ec3d864af43bfb0a94102e567ec3eb3ab6
Matthew Stadelman <stadelmanma@gmail.com>
2017-08-10 21:36:56 -0400

    Add inital_balance column to the budgets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714012758) do
+ActiveRecord::Schema.define(version: 20170811013522) do
 
   create_table "budget_expenses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.date     "date"
@@ -32,9 +32,10 @@ ActiveRecord::Schema.define(version: 20170714012758) do
 
   create_table "budgets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
     t.string   "name"
+    t.float    "initial_balance", limit: 24, default: 0.0
   end
 
   create_table "category_initializers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
