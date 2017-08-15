a80074bdc8206f159a388d36066bd57718ffcb6b
Matthew Stadelman <stadelmanma@gmail.com>
2017-08-14 20:31:42 -0400

    Create initial week totals view and concern logic

--- app/controllers/concerns/ledger_summary.rb
+++ app/controllers/concerns/ledger_summary.rb
@@ -29,10 +29,10 @@ module LedgerSummary
     #
     data = {
       weekly_average: weekly_average,
-      category_averages: {
-        # add some special logic to pull subcat from budgeted and deposits
-        # it would also be good to record the number of terms in each average
-      }
+      week_averaged_deposits: week_averaged_deposits(totals),
+      category_averages: category_averages(totals)
+      # add some special logic to pull subcat from budgeted and deposits
+      # it would also be good to record the number of terms in each average
     }
     #
     data
@@ -60,5 +60,46 @@ module LedgerSummary
 
   def sort_category_totals(data)
     Hash[data.to_a.sort { |a, b| b[1].abs <=> a[1].abs }]
+  end
+
+  def category_averages(totals)
+    # hash stores # values, # simple avg, # per week avg
+    data = Hash.new { |hash, key| hash[key] = [0, 0.0, 0.0] }
+    # step over all weekly totals in the ledger
+    totals.values.each do |week_total|
+      # pull in totals from each category excluding budgeted items and deposits
+      week_total[:category_totals].each do |key, amount|
+        next if key =~ /deposit|budgeted/i
+        #
+        data[key][0] += 1
+        data[key][1] += amount.to_f
+        data[key][2] += amount.to_f
+      end
+
+      # process budgeted items and deposit using subcategory as key
+      (week_total[:budgeted_expenses] + week_total[:deposits]).each do |trans|
+        key = trans.subcategory
+        data[key][0] += 1
+        data[key][1] += trans.amount.to_f
+        data[key][2] += trans.amount.to_f
+      end
+    end
+    # calculate averages
+    data.each do |key, cat_data|
+      sum = cat_data[1]
+      data[key][1] = sum / cat_data[0]
+      data[key][2] = sum / totals.length
+    end
+    # return overall averages for each category
+    data
+  end
+
+  def week_averaged_deposits(totals)
+    sum = 0.0
+    totals.values.each do |week_total|
+      week_total[:deposits].each { |trans| sum += trans.amount.to_f }
+    end
+    # average out
+    sum / totals.length
   end
 end
