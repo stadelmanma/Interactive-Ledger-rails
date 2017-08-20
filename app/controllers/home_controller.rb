154727b9ac9f4125ca44be634e126feca0a4b80e
Matthew Stadelman <stadelmanma@gmail.com>
2017-08-19 22:23:53 -0400

    Set Show Initializers url to nil

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     @page_links = [
       { name: 'New Budget', url: new_budget_path },
       { name: 'New Ledger', url: new_ledger_path },
-      { name: 'Show Initializers', url: category_initializers_path }
+      { name: 'Show Initializers', url: nil }
     ]
   end
 end
