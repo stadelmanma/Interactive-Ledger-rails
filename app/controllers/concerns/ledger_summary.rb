b8cd367a59ea6a49afb94b6f26eb5f058faaeb9b
Matthew Stadelman <stadelmanma@gmail.com>
2017-08-19 21:42:07 -0400

    Code clean up in ledger summary concern

--- app/controllers/concerns/ledger_summary.rb
+++ app/controllers/concerns/ledger_summary.rb
@@ -30,8 +30,6 @@ module LedgerSummary
       weekly_average: weekly_average,
       week_averaged_deposits: week_averaged_deposits(totals),
       category_averages: category_averages(totals)
-      # add some special logic to pull subcat from budgeted and deposits
-      # it would also be good to record the number of terms in each average
     }
     #
     data
@@ -68,22 +66,26 @@ module LedgerSummary
     totals.values.each do |week_total|
       # pull in totals from each category excluding budgeted items and deposits
       week_total.all_category_totals.each do |key, amount|
-        next if key =~ /deposit|budgeted/i
-        #
-        data[key][0] += 1
-        data[key][1] += amount
-        data[key][2] += amount
+        next if key.match?(/deposit|budgeted/i)
+        update_averages_entry(data, key, amount)
       end
 
       # process budgeted items and deposit using subcategory as key
       (week_total.budgeted_expenses + week_total.deposits).each do |trans|
-        key = trans.subcategory
-        data[key][0] += 1
-        data[key][1] += trans.amount
-        data[key][2] += trans.amount
+        update_averages_entry(data, trans.subcategory, trans.amount)
       end
     end
-    # calculate averages
+    # calculate averages and return data hash
+    calculate_averages(data)
+  end
+
+  def update_averages_entry(data, key, amount)
+    data[key][0] += 1
+    data[key][1] += amount
+    data[key][2] += amount
+  end
+
+  def calculate_averages(data)
     data.each do |key, cat_data|
       sum = cat_data[1]
       data[key][1] = sum / cat_data[0]
