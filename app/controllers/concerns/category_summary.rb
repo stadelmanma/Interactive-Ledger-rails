a3a13d79b1b204495d1a79f51b36b7f63d8d952f
Matthew Stadelman <stadelmanma@gmail.com>
2017-08-21 21:13:58 -0400

    Add average_per_week method CategorySummary class

--- app/controllers/concerns/category_summary.rb
+++ app/controllers/concerns/category_summary.rb
@@ -1,12 +1,13 @@
 # Manages a single set of data for a single category across a range of
 # transactions
 class CategorySummary
-  attr_accessor :name
+  attr_accessor :name, :nweeks
   attr_reader :transactions
 
-  def initialize(name, transactions = [])
+  def initialize(name, transactions = [], nweeks = 0)
     @name = name
     @transactions = transactions.find_all { |t| t.category.match?(/#{name}/i) }
+    @nweeks = nweeks
   end
 
   def <<(transaction)
@@ -18,7 +19,11 @@ class CategorySummary
   end
 
   def average(counts = nil)
-    counts = @transactions.length unless counts
+    counts = @transactions.length unless counts.nil?
     sum / counts
+  end
+
+  def average_per_week
+    average(@nweeks)
   end
 end
