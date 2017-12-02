3382d4e322ef9b11dad0efd59c940d99534db558
Matthew Stadelman <stadelmanma@gmail.com>
2017-12-02 10:40:15 -0500

    Change location of puma socket
    I needed to use a subdirectory to have write permissions.

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/var/www/#{fetch(:application)}"
-set :puma_bind,       "unix:///var/run/#{fetch(:application)}.sock"
+set :puma_bind,       "unix:///var/run/#{fetch(:application)}/#{fetch(:application)}.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
